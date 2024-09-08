3fccc3a9f5aa2cfbb9b53dee280f457d414fc421
박효찬 <qwes5674@gmail.com>
2024-09-08 20:23:27 +0900

    [Feat] 댓글 수정 기능

--- src/main/java/com/ohgiraffers/restapitest/comment/controller/CommentController.java
+++ src/main/java/com/ohgiraffers/restapitest/comment/controller/CommentController.java
@@ -73,20 +73,20 @@ public class CommentController {
                 .body(new ResponseMessage(HttpStatus.CREATED, savedComment.getPostId().getPostId()+"번 게시글의 댓글 등록 성공~!", responseMap));
     }
 
-//    // post 수정
-//    @Operation(summary = "댓글 수정", description = "우리 사이트 댓글 수정")
-//    @PutMapping("/{postId}")
-//    public ResponseEntity<ResponseMessage> modifyPost(@PathVariable int postId, @RequestBody CommentDTO modifyInfo) {
-//
-//        CommentEntity updatedPost = commentService.updatePost(postId, modifyInfo);
-//
-//        Map<String, Object> responseMap = new HashMap<>();
-//        responseMap.put("post", updatedPost);
-//
-//        return ResponseEntity.created(URI.create("/entity/TestEntity/" + postId))
-//                .body(new ResponseMessage(HttpStatus.OK, "댓글 수정 성공", responseMap));
-//    }
-//
+    // post 수정
+    @Operation(summary = "댓글 수정", description = "우리 사이트 댓글 수정")
+    @PutMapping("/{commentId}")
+    public ResponseEntity<ResponseMessage> modifyPost(@PathVariable int commentId, @RequestBody CommentDTO modifyInfo) {
+
+        CommentEntity updatedComment = commentService.updateComment(commentId, modifyInfo);
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("comment", updatedComment);
+
+        return ResponseEntity.created(URI.create("/entity/CommentEntity/" + commentId))
+                .body(new ResponseMessage(HttpStatus.OK, "댓글 수정 성공", responseMap));
+    }
+
 //    // 댓글 삭제
 //    @Operation(summary = "댓글 삭제")
 //    @ApiResponses({
--- src/main/java/com/ohgiraffers/restapitest/comment/service/CommentService.java
+++ src/main/java/com/ohgiraffers/restapitest/comment/service/CommentService.java
@@ -52,20 +52,20 @@ public class CommentService {
         return commentRepository.save(commentEntity);
     }
 
-//    // 댓글 수정
-//    public CommentEntity updatePost(int postId, CommentDTO modifyInfo) {
-//
-//        CommentEntity post= commentRepository.findById(postId)
-//                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다. postId: " + postId));
-//
-//        post = post.toBuilder()
-//                .title(modifyInfo.getTitle())
-//                .content(modifyInfo.getContent())
-//                .build();
-//
-//        return commentRepository.save(post);
-//    }
-//
+    // 댓글 수정
+    public CommentEntity updateComment(int commentId, CommentDTO modifyInfo) {
+
+        CommentEntity comment= commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다. comment: " + commentId));
+
+        comment = comment.toBuilder()
+                .postId(modifyInfo.getPostId())
+                .content(modifyInfo.getContent())
+                .build();
+
+        return commentRepository.save(comment);
+    }
+
 //    // 댓글 삭제
 //    public void deletePostById(int postId) {
 //
